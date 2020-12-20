805f2a1b827723cc3d88035d352fa04bb1471cc2
abama cai <caicaiking@gmail.com>
2020-12-20 22:33:50 +0800

    Change the source list and target

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,4 @@ int main(int argc, char **argv)
     simple_ss::core::task_manager::get_instance().emit(1, extcom_customer_data);
     simple_ss::core::task_manager::get_instance().run(1);
     simple_ss::core::task_manager::get_instance().run(1);
-    std::cout << "ends " << std::endl;
 }
